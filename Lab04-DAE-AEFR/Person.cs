4be63c808c1bc348b203c8a043907ed0c2bea5a4
Alumno <Alumno@LAB707-11>
2023-04-10 22:12:03 -0500

    Agregar archivos de proyecto.

---
+++ Lab04-DAE-AEFR/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab04_DAE_AEFR
+{
+    public class Person
+    {
+        public string PersonId
+        {
+            get; set;
+        }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName { get; set; }
+
+        public int Age {get;set;}
+
+    
+    }
+}
